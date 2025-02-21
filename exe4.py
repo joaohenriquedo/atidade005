5d96d0babfa74630f3520590bd7d7668034e2ac4
joaohenriquedo <joaohenriquedomingos2008@gmail.com>
2025-02-21 15:34:49 -0300

    update

--- exe4.py
+++ exe4.py
@@ -1,3 +1,7 @@
+#Faça um programa que peça o nome do convidado que o usuario deseja convidar para uma festa. Depois disso, exiba a mensagem "[nome] foi adicionado(a) com sucesso no convite!" e adicione 1 à contagem.
+#Em seguida, pergunte se ele quer convidar outra pessoa.
+#Continue repetindo isso até que ele não queira mais convidar ninguém para a festa e, em seguida, mostre quantas pessoas foram convidas para a festa.
+
 contagem_convidados = 0
 resposta = 's'
 while resposta.lower() == 's':  
